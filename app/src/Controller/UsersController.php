c98620e3c237846452a7074efbb80e0e43025ab9
donc91 <mr.do61@gmail.com>
2024-01-31 23:32:44 +0700

    install authentication and jwt

---
+++ app/src/Controller/UsersController.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Cake\Event\EventInterface;
+use Cake\Utility\Security;
+use Firebase\JWT\JWT;
+
+/**
+ * Users Controller
+ *
+ * @property \Authentication\Controller\Component\AuthenticationComponent $Authentication
+ */
+class UsersController extends AppController
+{
+    public function beforeFilter(EventInterface $event)
+    {
+        parent::beforeFilter($event);
+
+        $this->Authentication->allowUnauthenticated(['login', 'register']);
+    }
+
+    public function login()
+    {
+        $result = $this->Authentication->getResult();
+        if ($result->isValid()) {
+            $user = $result->getData();
+            $payload = [
+                'sub' => $user->id,
+                'exp' => time() + 60,
+            ];
+
+            $json = [
+                'token' => JWT::encode($payload, Security::getSalt(), 'HS256'),
+            ];
+        } else {
+            $this->response = $this->response->withStatus(401);
+            $json = [];
+        }
+
+        $this->set(compact('json'));
+        $this->viewBuilder()->setOption('serialize', 'json');
+    }
+
+    public function logout()
+    {
+        $json = [];
+
+        $this->Authentication->logout();
+
+        $this->set(compact('json'));
+        $this->viewBuilder()->setOption('serialize', 'json');
+    }
+}
